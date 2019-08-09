e3813482370ab740f422cf9d077eb3e78b31a0d1
Erik Schjolberg <erik.schjolberg@gmail.com>
2019-08-08 17:12:52 -0700

    commit

---
+++ Hack Reactor/Placement.js
@@ -0,0 +1,10 @@
+// on the line below, declare a variable named myString,
+// then assign it to any string of your choosing
+
+const myString = "Erik"
+
+// on the line below, declare a variable named myArray,
+// then assign it to an array with 3 elements
+
+let myArray = ['Fred', 'Julie', 12]
+
